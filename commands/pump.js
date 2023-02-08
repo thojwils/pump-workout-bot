d3f0fa105b14290cfd8de1e31ab877bb351b5820
TJ Wilson <87959418+thojwils@users.noreply.github.com>
2023-02-07 21:47:13 -0500

    Creating Pump Bot

---
+++ commands/pump.js
@@ -0,0 +1,51 @@
+const { SlashCommandBuilder } = require("discord.js");
+const { Client, GatewayIntentBits } = require("discord.js");
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("pump")
+    .setDescription("Saves a pump"),
+  async execute(interaction) {
+    await interaction.reply("Good Pump! ✅");
+    const MongoClient = require("mongodb").MongoClient;
+
+    const url =
+      "mongodb+srv://thojwils:hVH3z4YMTyBldh6t@cluster0.ie3kmmd.mongodb.net/test";
+    const dbName = "workouts";
+
+    let date;
+    const username = interaction.user.username;
+    console.log(username);
+    if (username) {
+      let mongoClient;
+
+      MongoClient.connect(url, { useUnifiedTopology: true })
+        .then((_client) => {
+          console.log("MongoDB connection started");
+          mongoClient = _client;
+          const db = mongoClient.db(dbName);
+          const collection = db.collection("workoutData");
+          // insert date
+          date = new Date();
+          return collection.insertOne({
+            date: date,
+            username: username,
+          });
+        })
+        .then((res) => {
+          console.log(`Date added: ${date}`);
+          return mongoClient.close();
+        })
+        .then(() => {
+          console.log("MongoDB connection closed");
+        })
+        .catch((err) => {
+          console.error(err);
+          return mongoClient.close();
+        });
+    } else {
+      username = "Unknown";
+    }
+  },
+};
---
+++ commands/workedout.js
@@ -0,0 +1,92 @@
+const { SlashCommandBuilder } = require("discord.js");
+const { Client, GatewayIntentBits } = require("discord.js");
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const MongoClient = require("mongodb").MongoClient;
+const { Chart } = require("chart.js");
+const Canvas = require("canvas");
+
+const url =
+  "mongodb+srv://thojwils:hVH3z4YMTyBldh6t@cluster0.ie3kmmd.mongodb.net/test";
+const dbName = "workouts";
+
+const fetchWorkoutDataForUser = async (username) => {
+  let mongoClient;
+  const workouts = [];
+  try {
+    mongoClient = await MongoClient.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    const db = mongoClient.db(dbName);
+    const collection = db.collection("workoutData");
+
+    // retrieve list of workouts for the given user in the current month
+    const currentMonth = new Date().getMonth();
+    const res = await collection
+      .find({
+        username: username,
+        date: {
+          $gte: new Date(new Date().setMonth(currentMonth, 1)),
+          $lt: new Date(new Date().setMonth(currentMonth + 1, 1)),
+        },
+      })
+      .toArray();
+
+    res.forEach((workout) => {
+      workouts.push(workout);
+    });
+  } catch (error) {
+    console.error(error);
+  } finally {
+    if (mongoClient) {
+      await mongoClient.close();
+    }
+  }
+  return workouts;
+};
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("show-workouts")
+    .setDescription("Shows user's workout data"),
+  async execute(interaction) {
+    const username = interaction.user.username;
+
+    // Fetch user's workout data from database
+    const workoutData = await fetchWorkoutDataForUser(username);
+
+    // Create the chart using Chart.js
+    const canvas = Canvas.createCanvas(700, 500);
+    const ctx = canvas.getContext("2d");
+    const chart = new Chart(ctx, {
+      type: "bar",
+      data: {
+        labels: workoutData.map((entry) => entry.date),
+        datasets: [
+          {
+            label: "Workouts",
+            data: workoutData.map((entry) => entry.value),
+            backgroundColor: "rgba(54, 162, 235, 0.2)",
+            borderColor: "rgba(54, 162, 235, 1)",
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+              },
+            },
+          ],
+        },
+      },
+    });
+
+    // Send the chart as a message
+    interaction.respond(`Here's your workout data for the current month:`);
+    interaction.respond({ files: [chart.toBase64Image()] });
+  },
+};
