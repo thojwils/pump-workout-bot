45d36f60411c314df5d918aab8a1b4a1638c2c13
TJ Wilson <87959418+thojwils@users.noreply.github.com>
2023-02-14 19:54:04 -0500

    reverted fully

--- commands/pump.js
+++ commands/pump.js
@@ -1,107 +1,54 @@
 const { SlashCommandBuilder } = require("discord.js");
 const { Client, GatewayIntentBits } = require("discord.js");
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-const MongoClient = require("mongodb").MongoClient;
-
-const url =
-  "mongodb+srv://thojwils:hVH3z4YMTyBldh6t@cluster0.ie3kmmd.mongodb.net/test";
-const dbName = "workouts";
-const collectionName = "workoutData";
-
-const pumpData = new SlashCommandBuilder()
-  .setName("pump")
-  .setDescription("Saves a pump with type")
-  .addStringOption((option) =>
-    option
-      .setName("type")
-      .setDescription("The type of workout")
-      .setRequired(true)
-      .addChoices(
-        { name: "Full body", value: "full_body" },
-        { name: "Upper body", value: "upper_body" },
-        { name: "Lower body", value: "lower_body" },
-        { name: "Cardio", value: "cardio" },
-        { name: "Yoga", value: "yoga" },
-        { name: "Stretch", value: "stretch" },
-        { name: "Rest", value: "rest" }
-      )
-  );
-
-const saveWorkoutData = async (username, date, type) => {
-  try {
-    const client = await MongoClient.connect(url, {
-      useUnifiedTopology: true,
-    });
-    const db = client.db(dbName);
-    const collection = db.collection(collectionName);
-
-    await collection.createIndex({ username: 1, date: -1 }, { unique: true });
-
-    const workoutData = { date, username, type };
-    const existingData = await collection.findOne({
-      username: username,
-      date: {
-        $gt: new Date(Date.now() - 24 * 60 * 60 * 1000),
-        $lt: new Date(Date.now()),
-      },
-    });
-    if (!existingData) {
-      await collection.insertOne(workoutData);
-      console.log(`Data added: ${JSON.stringify(workoutData)}`);
-    } else {
-      console.log(
-        `Data already exists for this user today: ${JSON.stringify(
-          existingData
-        )}`
-      );
-    }
-  } catch (error) {
-    console.error(error);
-  } finally {
-    if (client) {
-      client.close();
-    }
-  }
-};
-
-const processedInteractions = new Set();
-
-const markInteractionAsProcessed = (interaction) => {
-  processedInteractions.add(interaction.id);
-  setTimeout(() => {
-    processedInteractions.delete(interaction.id);
-  }, 1000 * 60 * 60 * 24); // Remove from set after 24 hours
-};
 
 module.exports = {
-  data: pumpData,
+  data: new SlashCommandBuilder()
+    .setName("pump")
+    .setDescription("Saves a pump"),
   async execute(interaction) {
-    // Check if the interaction has already been replied to or deferred
-    if (interaction.replied || processedInteractions.has(interaction.id)) {
-      return;
+    await interaction.reply("Good Pump! ✅");
+    const MongoClient = require("mongodb").MongoClient;
+
+    const url =
+      "mongodb+srv://thojwils:hVH3z4YMTyBldh6t@cluster0.ie3kmmd.mongodb.net/test";
+    const dbName = "workouts";
+
+    let date;
+    const username = interaction.user.username;
+    console.log(username);
+    if (username) {
+      let mongoClient;
+
+      MongoClient.connect(url, { useUnifiedTopology: true })
+        .then((_client) => {
+          console.log("MongoDB connection started");
+          mongoClient = _client;
+          const db = mongoClient.db(dbName);
+          const collection = db.collection("workoutData");
+          // insert date
+          date = new Date().toLocaleString("en-US", {
+            timeZone: "America/New_York",
+          });
+
+          return collection.insertOne({
+            date: date,
+            username: username,
+          });
+        })
+        .then((res) => {
+          console.log(`Date added: ${date}`);
+          return mongoClient.close();
+        })
+        .then(() => {
+          console.log("MongoDB connection closed");
+        })
+        .catch((err) => {
+          console.error(err);
+          return mongoClient.close();
+        });
+    } else {
+      username = "Unknown";
     }
-
-    // Check that interaction contains a user property
-    if (!interaction.user) {
-      await interaction.reply(
-        "Error: interaction does not contain user information."
-      );
-      return;
-    }
-
-    // Extract user information from interaction
-    const username = interaction.user.username || "Unknown";
-    const date = new Date().toLocaleString("en-US", {
-      timeZone: "America/New_York",
-    });
-    const type = interaction.options.getString("type");
-
-    // Perform the discord API call and MongoDB insert asynchronously
-    await interaction.deferReply();
-    await Promise.all([
-      interaction.editReply(`Good Pump <@${username}>! ✅`),
-      saveWorkoutData(username, date, type),
-    ]);
-    markInteractionAsProcessed(interaction);
   },
 };
