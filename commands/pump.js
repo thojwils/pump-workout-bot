9d6377c9c0df6b5aa86fabbfc2a1d733695a2edb
TJ Wilson <87959418+thojwils@users.noreply.github.com>
2023-02-16 23:08:09 -0500

    added streak command and changes to pump command

--- commands/pump.js
+++ commands/pump.js
@@ -1,54 +1,84 @@
-const { SlashCommandBuilder } = require("discord.js");
-const { Client, GatewayIntentBits } = require("discord.js");
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const moment = require("moment");
+const { MongoClient } = require("mongodb");
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("pump")
-    .setDescription("Saves a pump"),
+    .setName("streak")
+    .setDescription("Counts how many consecutive days you have worked out"),
   async execute(interaction) {
-    await interaction.reply("Good Pump! ✅");
-    const MongoClient = require("mongodb").MongoClient;
-
+    const username = interaction.user.username;
     const url =
       "mongodb+srv://thojwils:hVH3z4YMTyBldh6t@cluster0.ie3kmmd.mongodb.net/test";
-    const dbName = "workouts";
+    const dbName = "workouts_dev";
+    let streak = 0;
 
-    let date;
-    const username = interaction.user.username;
-    console.log(username);
-    if (username) {
-      let mongoClient;
+    try {
+      const client = new MongoClient(url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      await client.connect();
+      const db = client.db(dbName);
+      const collection = db.collection("workoutData");
 
-      MongoClient.connect(url, { useUnifiedTopology: true })
-        .then((_client) => {
-          console.log("MongoDB connection started");
-          mongoClient = _client;
-          const db = mongoClient.db(dbName);
-          const collection = db.collection("workoutData");
-          // insert date
-          date = new Date().toLocaleString("en-US", {
-            timeZone: "America/New_York",
-          });
+      const workouts = await collection
+        .find({ username })
+        .sort({ date: 1 })
+        .toArray();
+      if (workouts.length > 0) {
+        streak++;
+        const today = moment().startOf("day");
+        let lastWorkoutDate = moment(
+          workouts[0].date,
+          "M/D/YYYY, h:mm:ss A"
+        ).startOf("day");
+        for (let i = 1; i < workouts.length; i++) {
+          const workoutDate = moment(
+            workouts[i].date,
+            "M/D/YYYY, h:mm:ss A"
+          ).startOf("day");
+          const diffDays = workoutDate.diff(lastWorkoutDate, "days");
+          if (diffDays === 1) {
+            streak++;
+            console.log(streak);
+          } else {
+            break;
+          }
+          lastWorkoutDate = workoutDate;
+        }
+        if (streak === 0) {
+          // If there was a gap between the last workout and today, the streak is broken
+          const diffDays = today.diff(lastWorkoutDate, "days");
+          if (diffDays === 1) {
+            streak = 1;
+          }
+        }
+      }
 
-          return collection.insertOne({
-            date: date,
-            username: username,
-          });
-        })
-        .then((res) => {
-          console.log(`Date added: ${date}`);
-          return mongoClient.close();
-        })
-        .then(() => {
-          console.log("MongoDB connection closed");
-        })
-        .catch((err) => {
-          console.error(err);
-          return mongoClient.close();
-        });
-    } else {
-      username = "Unknown";
+      // set fire
+      let fire = `🔥`;
+      let fireStreak = "";
+
+      for (let i = 0; i < streak; i++) {
+        fireStreak += fire;
+      }
+
+      const embedBuilder = new EmbedBuilder()
+        .setTitle("Workout Streak")
+        .setColor("#0099ff")
+        .setDescription(
+          `Your current workout streak is ${streak} day${
+            streak === 1 ? "" : "s"
+          }. ${fireStreak}`
+        );
+      await interaction.reply({ embeds: [embedBuilder] });
+    } catch (error) {
+      console.error(error);
+      await interaction.reply({
+        content: "An error occurred while executing the command.",
+        ephemeral: true,
+      });
     }
   },
 };
