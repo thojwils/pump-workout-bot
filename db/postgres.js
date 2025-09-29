be27007a84398762adc05e7d70f774af965ff702
Thomas Wilson <tjwilson411@yahoo.com>
2025-09-28 23:45:52 -0400

    Update postgres.js

--- db/postgres.js
+++ db/postgres.js
@@ -129,7 +129,6 @@ async function query(text, params) {
     const start = Date.now();
     const res = await pool.query(text, params);
     const duration = Date.now() - start;
-    // console.log('Executed query', { text, duration: `${duration}ms`, rows: res.rowCount });
     return res;
 }
 
